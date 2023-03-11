22b0f4aae59fe1e33affb8572f6ef5f92fe42b3e
uberney <uberney.zapatap@perficient.com>
2023-03-11 10:55:59 -0500

    fix issue

--- model/Vendedor/Vendedor.py
+++ model/Vendedor/Vendedor.py
@@ -1,3 +1,5 @@
+from model.Persona.Persona import Persona
+
 class Vendedor(Persona):
     def __init__(self, cedula, nombre, edad, usuario, contrasena):
         super().__init__(cedula, nombre, edad, "Vendedor")
