ed99511687bce25d194020cb529c3bc363dd2203
uberney <uberney.zapatap@perficient.com>
2023-03-11 10:40:41 -0500

    ordenamiento de carpetas, de esta manera sera mucho mas facil identificar que reglas de negocio corresponden a cada uno de los modelos

---
+++ model/Mascota/Mascota.py
@@ -0,0 +1,10 @@
+class Mascota:
+    def __init__(self, nombre, cedula_dueño, edad, especie, raza, caracteristicas, peso):
+        self.nombre = nombre
+        self.cedula_dueño = cedula_dueño
+        self.edad = edad
+        self.id = None 
+        self.especie = especie
+        self.raza = raza
+        self.caracteristicas = caracteristicas
+        self.peso = peso
---
+++ model/Vendedor/Vendedor.py
@@ -0,0 +1,5 @@
+class Vendedor(Persona):
+    def __init__(self, cedula, nombre, edad, usuario, contrasena):
+        super().__init__(cedula, nombre, edad, "Vendedor")
+        self.usuario = usuario
+        self.contrasena = contrasena
