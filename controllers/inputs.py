143beb23d2e6ca52f724d82da61667358dfa7ec8
Andrespc <anfega@outlokk.es>
2023-03-11 20:58:07 -0500

    se crea controlador para agregar medico o vendedor

---
+++ controllers/inputs.py
@@ -0,0 +1,32 @@
+from model.Persona import PersonaBusiness
+
+def addEmpleado(veterinaria,nombre,cedula,edad,rol):
+    if nombre== None or nombre == " ":
+        print("el nombre no puede ser un espacio vacio")
+        return
+    
+    if cedula == None or cedula == " ":
+        print("la cedula no puede ser vacio")
+        return
+    try:
+        cedula = int(cedula)
+    except:
+        print("la cedula debe ser numerica")
+        return
+    if edad == None or edad == " ":
+        print("la edad no puede ser un espacio vacio")
+        return
+    try:
+        edad = int(edad)
+    except:
+        print("la edad debe ser numerica")
+        return
+    if rol == None or rol== " ":
+        print("rol no puede ser un espacio vacio")
+        return
+    if rol !="MEDICO" or rol !="VENDEDOR":
+        print("el rol debe ser MEDICO o VENDEDOR")
+        return
+    print("validacion exitosa")
+    PersonaBusiness.AfiliarEmpleado(veterinaria,nombre,cedula,edad,rol)
+
