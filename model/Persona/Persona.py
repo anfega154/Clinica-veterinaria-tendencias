4ed519f12b7a3d8f64d45d5f343bb910cd9d3a0e
Nataly <natalymejia13@gmail.com>
2023-03-21 23:54:23 -0500

    Cree metodo para consultar vendedor, cliente y veterinario

--- model/Persona/Persona.py
+++ model/Persona/Persona.py
@@ -4,3 +4,15 @@ class Persona:
         self.nombre = nombre
         self.edad = edad
         self.rol = rol
+
+    def get_tipo(self):
+        return self.rol
+
+    def es_Vendedor(self):
+        return self.rol == "Vendedor"
+
+    def es_DueñoMascota(self):
+        return self.rol == "DueñoMascota"
+
+    def es_MedicoVeterinario(self):
+        return self.rol == "MedicoVeterinario"
